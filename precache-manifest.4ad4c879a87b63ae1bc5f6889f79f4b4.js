5b4b292a4d9aa032b7aa5b53119f0b0ce2b201de
Shafikul Islam <shafikul.me@gmail.com>
2020-10-19 12:33:58 +0600

    Updates

---
+++ precache-manifest.4ad4c879a87b63ae1bc5f6889f79f4b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d85c967e6937f87c1651be5e47a984",
+    "url": "/react-org-chart/index.html"
+  },
+  {
+    "revision": "b2a1dd0067c0fcffe7b8",
+    "url": "/react-org-chart/static/css/main.047fb8b0.chunk.css"
+  },
+  {
+    "revision": "bc24a2d4130da987a160",
+    "url": "/react-org-chart/static/js/2.a9e5a11c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-org-chart/static/js/2.a9e5a11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a1dd0067c0fcffe7b8",
+    "url": "/react-org-chart/static/js/main.e7e5ebc2.chunk.js"
+  },
+  {
+    "revision": "bb41dfa41bd2945e9ca534fcd6227399",
+    "url": "/react-org-chart/static/js/main.e7e5ebc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf929761cfdd5e81a7f",
+    "url": "/react-org-chart/static/js/runtime-main.36560262.js"
+  },
+  {
+    "revision": "f8399445e0f391bfe9059f4b992b3657",
+    "url": "/react-org-chart/static/media/avatar-personnel.f8399445.svg"
+  }
+]);
